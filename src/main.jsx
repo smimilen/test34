0868a420567d4c7215e85e632a54978322a269e3
smimilen <93482945+smimilen@users.noreply.github.com>
2026-04-26 05:01:57 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,9 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import gsap from 'gsap'
 import ScrollTrigger from 'gsap/ScrollTrigger'
 import './index.css'
 import App from './App.jsx'
 
-// Register GSAP plugins once globally
 gsap.registerPlugin(ScrollTrigger)
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById('root')).render(<App />)
